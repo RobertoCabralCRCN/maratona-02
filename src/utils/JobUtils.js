25022da6ebeb8fcbcc6a87546cef14625205bee2
Roberto Cabral <croberto.cabral@yahoo.com.br>
2021-10-08 16:08:43 -0300

    t remote add origin git@github.com:RobertoCabralCRCN/maratona-02.gitInitial Commit

---
+++ src/utils/JobUtils.js
@@ -0,0 +1,23 @@
+module.exports = {
+    remainingDays(job) {
+     // ajustes no jobs
+     //cauclo de tempo  restante
+     const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+   
+     const createdDate = new Date(job.created_at)
+     const dueDay = createdDate.getDate() + Number(remainingDays)
+     const dueDateInMs = createdDate.setDate(dueDay)
+   
+     const timeDiffInMs = dueDateInMs - Date.now()
+     // transformar Milissegundos em Day 
+     const dayInMs = 1000 * 60 * 60 * 24
+     const dayDiff = Math.ceil(timeDiffInMs / dayInMs)
+   
+   
+     //restam X dias
+     return dayDiff
+   },
+
+   calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+   
+ }
